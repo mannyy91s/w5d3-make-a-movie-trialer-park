b5f0392e637ceb8e8e8fb5b63ff96e73aa2ab5c7
Manpreet Singh <mannyy91s@gmail.com>
2016-10-26 13:27:56 -0400

    added 4 movie trailer buttons and finished explorer mode of assignment

--- js/global.js
+++ js/global.js
@@ -1,16 +1,28 @@
 // 1. Make a class called Trailer that extends Movie
-class _____ extends _____ {
+class Trailer extends Movie {
 
     // 2. Inside the class, add a constructor function that takes two arguments, a name and an id
-    constructor() {
+    constructor(name, id) {
 
         // 3. Call the parent constructor function using the super function
-
+        super()
         // 4. Assign a name and id instance property to the incoming name and id arguments
-
+        this.name = name
+        this.id = id
         // 5. This method will show the button on the screen, no changes necessary
         this.renderButton()
     }
 }
 
 // 6. Create four instances of the Trailer class, passing in the name of a film as the first argument and its corresponding YouTube ID as the second argument
+//Instance 1
+var movieTrailer = new Trailer('John Wick 2', 'nMqETeQrgqU')
+
+//Instance 2
+var movieTrailer = new Trailer('P.O.C 5', 'lsJ58L3u8qw')
+
+//Instance 3
+var movieTrailer = new Trailer('Max Steal', 'Tf4sa0BVJVw')
+
+//Instance 4
+var movieTrailer = new Trailer('Justice League', 'fIHH5-HVS9o')
